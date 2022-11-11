b37a085159297ea43a3bc1015a91a96ebd8d2d04
vuvandung32 <108406047+vuvandung32@users.noreply.github.com>
2022-11-11 13:40:07 +0700

    Add files via upload

---
+++ Btb1Application.java
@@ -0,0 +1,13 @@
+package com.example.btb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Btb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Btb1Application.class, args);
+	}
+
+}
